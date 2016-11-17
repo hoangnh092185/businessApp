57deb6ee189b03d36c3c907f380a84bc08abda44
Nhate Hoang and Vanessa Palacios Sharma <nhate+vanessa@>
2016-11-17 09:30:23 -0800

    Direct to new activity AboutActivity.java

---
+++ app/src/main/java/com/epicodus/businessapp/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.epicodus.businessapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.security.DomainCombiner;
+
+public class AboutActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    }
--- app/src/main/java/com/epicodus/businessapp/MainActivity.java
+++ app/src/main/java/com/epicodus/businessapp/MainActivity.java
@@ -1,13 +1,40 @@
 package com.epicodus.businessapp;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mAppNameTextView;
+    private Button mExploreAdxButton;
+//    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface old_stamper = Typeface.createFromAsset(getAssets(), "fonts/old_stamper.ttf");
+        mAppNameTextView.setTypeface(old_stamper);
+
+
+            mExploreAdxButton = (Button) findViewById(R.id.exploreAdxButton);
+            mExploreAdxButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                    startActivity(intent);
+                }
+
+            });
+
+        }
+
     }
-}
+
